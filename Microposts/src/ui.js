ed564c0986cd06540cde70a2f5f1a6e4f78f6917
dev <my@emeil.com>
2019-01-23 16:45:48 +0300

    Adding submitPost function, UI features

--- Microposts/src/ui.js
+++ Microposts/src/ui.js
@@ -2,17 +2,17 @@ class UI {
     constructor() {
         this.post = document.querySelector('.posts');
         this.titleInput = document.querySelector('#title');
-        this.bodyInput = document.querySelector('.body');
+        this.bodyInput = document.querySelector('#body');
         this.idInput = document.querySelector('#id');
         this.postSubmit = document.querySelector('.post-submit');
         this.forState = 'add';
 
     }
     showPosts(posts) {
-    let output = '';
+        let output = '';
 
-    posts.forEach((post) => {
-        output += `
+        posts.forEach((post) => {
+            output += `
             <div class="card mb-3">
                 <div class="card-body">
                     <h4 class="card-title">${post.title}</h4>
@@ -26,11 +26,37 @@ class UI {
                 </div>
             </div>
         `;
-    });
+        });
 
-    this.post.innerHTML = output;
+        this.post.innerHTML = output;
+    }
+    showAlert(message, className) {
+        this.clearAlert();
 
-  }
+        const div = document.createElement('div');
+        div.className = className;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.postsContainer');
+        const posts = document.querySelector('.posts');
+        container.insertBefore(div, posts);
+
+        setTimeout(() => {
+            this.clearAlert();
+        }, 3000);
+    }
+
+    clearAlert() {
+        const currentAlert = document.querySelector('.alert');
+
+        if (currentAlert) {
+            currentAlert.remove();
+        }
+    }
+
+    clearFields() {
+        this.titleInput.value = '';
+        this.bodyInput.value = '';
+    }
 }
 
 
